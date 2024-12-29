87285fcbba164db989c2b5d150f925f2b6debf36
h1un <h1unha@naver.com>
2024-12-30 03:07:04 +0900

    feat : tailwind.config.ts 동적 class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,12 @@ export default {
   ],
   theme: {
   	extend: {
+		safelist: [
+			{
+				pattern: /(bg|text)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose|periwinkle')-(50|100|200|300|400|500|600|700|800|900|950')/,
+				variants: ['hover'],
+			}
+		],
   		colors: {
 			periwinkle: {
 				50: '#f5f5fe',
